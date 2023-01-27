9b911319ea58c183504dc5d781e2f197a82b2a60
Сергей Челпанов <S.inter.4@mail.ru>
2023-01-28 00:00:48 +0300

    Задача_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,3 +3,20 @@
 Console.Write("Введите число А ");
 int numberA = int.Parse(Console.ReadLine()!);
 
+Console.Write("Введите число B ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA > numberB) 
+{
+    Console.WriteLine($"Большее число {numberA}");
+    Console.WriteLine($"Меньшее число {numberB}");
+}
+if (numberA < numberB)
+{
+    Console.WriteLine($"Большее число {numberB}");
+    Console.WriteLine($"Меньшее число {numberA}");
+}
+else if (numberA == numberB)
+ {
+    Console.WriteLine($"Числа равны {numberA} = {numberB}");
+ }
